b290f65d9129411e0b2df376ad242c6c5ba685ec
V3RM1N <v3rm1n0@proton.me>
2025-09-29 13:12:12 +0200

    feat: Add nixd server configuration and formatter support

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,25 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "nixd" }
 vim.lsp.enable(servers)
 
+
+vim.lsp.config('nixd', {
+  cmd = { "nixd" },
+  settings = {
+    nixd = {
+      nixpkgs = {
+        expr = "import <nixpkgs> { }",
+      },
+      formatting = {
+        command = { "nixfmt" }, -- or nixfmt or nixpkgs-fmt
+      },
+      options = {
+        nixos = {
+        expr = '(builtins.getFlake "/etc/nixos/").nixosConfigurations',
+        },
+      },
+    },
+  },
+})
 -- read :h vim.lsp.config for changing options of lsp servers 
