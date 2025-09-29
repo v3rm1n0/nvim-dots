0e5e8fdb0d4c23e9c84873fb88812ed25b4d09ff
V3RM1N <v3rm1n0@proton.me>
2025-09-29 13:12:16 +0200

    feat: Add VimTeX plugin configuration

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -34,4 +34,17 @@ return {
     priority = 49,
   },
 
+  {
+    "lervag/vimtex",
+    lazy = false, -- uncomment to pin to a specific release
+    init = function()
+    -- VimTeX configuration goes here, e.g.
+    vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_view_forward_search_on_start = false
+			vim.g.vimtex_compiler_latexmk = {
+			aux_dir = "/home/v3rm1n/.texfiles/",
+			out_dir = "/home/v3rm1n/.texfiles/",
+		}
+    end
+  },
 }
