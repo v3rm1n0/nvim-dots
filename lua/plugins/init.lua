1ebc9b5c9a0e29d34852ad1257d4f1699fc7c894
V3RM1N <v3rm1n0@proton.me>
2025-10-27 15:07:27 +0100

    feat: Added markdown export

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -67,22 +67,7 @@ return {
     lazy = false,
     version = "*",
     config = function()
-      require("neorg").setup {
-        load = {
-          ["core.defaults"] = {},
-          ["core.concealer"] = {},
-          ["core.dirman"] = {
-            config = {
-              workspaces = {
-                notes = "~/notes",
-              },
-              default_workspace = "notes",
-            },
-          },
-        },
-      }
-      vim.wo.foldlevel = 99
-      vim.wo.conceallevel = 2
+      require "configs.neorg"
     end,
   },
 }
